8889521fbe401d996f9bf7367e5b745de913707f
jjocalero-alt <jjocalero@uamv.edu.ni>
2026-06-03 14:29:22 -0600

    Modulo 2/Alteración de tablas (Restricciones)

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -70,3 +70,91 @@ CREATE TABLE Medicamentos (
     fecha_vencimiento DATE      NULL
 );
 GO
+
+--Modulo 2
+ALTER TABLE Pacientes
+    ADD CONSTRAINT PK_Pacientes PRIMARY KEY (paciente_id);
+GO
+ 
+-- 2. Definir PRIMARY KEY en Médicos
+ALTER TABLE Medicos
+    ADD CONSTRAINT PK_Medicos PRIMARY KEY (medico_id);
+GO
+ 
+ALTER TABLE Especialidades
+    ADD CONSTRAINT PK_Especialidades PRIMARY KEY (especialidad_id);
+GO
+ 
+ALTER TABLE Citas
+    ADD CONSTRAINT PK_Citas PRIMARY KEY (cita_id);
+GO
+ 
+ALTER TABLE Habitaciones
+    ADD CONSTRAINT PK_Habitaciones PRIMARY KEY (habitacion_id);
+GO
+ 
+ALTER TABLE Tratamientos
+    ADD CONSTRAINT PK_Tratamientos PRIMARY KEY (tratamiento_id);
+GO
+ 
+ALTER TABLE Medicamentos
+    ADD CONSTRAINT PK_Medicamentos PRIMARY KEY (medicamento_id);
+GO
+ 
+ALTER TABLE Pacientes
+    ALTER COLUMN nombre VARCHAR(100) NOT NULL;
+GO
+ 
+ALTER TABLE Medicos
+    ALTER COLUMN nombre VARCHAR(100) NOT NULL;
+GO
+ 
+ALTER TABLE Pacientes
+    ADD CONSTRAINT UQ_Pacientes_Correo UNIQUE (correo);
+GO
+ 
+ALTER TABLE Medicos
+    ADD CONSTRAINT UQ_Medicos_Correo UNIQUE (correo);
+GO
+ 
+ALTER TABLE Pacientes
+    ADD CONSTRAINT CHK_Pacientes_Edad CHECK (edad >= 0);
+GO
+ 
+ALTER TABLE Medicos
+    ADD CONSTRAINT CHK_Medicos_Salario CHECK (salario > 0);
+GO
+ 
+ALTER TABLE Pacientes
+    ADD CONSTRAINT DF_Pacientes_FechaRegistro DEFAULT GETDATE() FOR fecha_registro;
+GO
+ 
+ALTER TABLE Medicos
+    ADD CONSTRAINT FK_Medicos_Especialidades
+    FOREIGN KEY (especialidad_id) REFERENCES Especialidades(especialidad_id);
+GO
+ 
+ALTER TABLE Citas
+    ADD CONSTRAINT FK_Citas_Pacientes
+    FOREIGN KEY (paciente_id) REFERENCES Pacientes(paciente_id);
+GO
+ 
+ALTER TABLE Citas
+    ADD CONSTRAINT FK_Citas_Medicos
+    FOREIGN KEY (medico_id) REFERENCES Medicos(medico_id);
+GO
+ 
+ALTER TABLE Tratamientos
+    ADD CONSTRAINT FK_Tratamientos_Pacientes
+    FOREIGN KEY (paciente_id) REFERENCES Pacientes(paciente_id);
+GO
+ 
+ALTER TABLE Medicamentos
+    ADD CONSTRAINT FK_Medicamentos_Tratamientos
+    FOREIGN KEY (tratamiento_id) REFERENCES Tratamientos(tratamiento_id);
+GO
+ 
+ALTER TABLE Habitaciones
+    ADD CONSTRAINT FK_Habitaciones_Pacientes
+    FOREIGN KEY (paciente_id) REFERENCES Pacientes(paciente_id);
+GO
